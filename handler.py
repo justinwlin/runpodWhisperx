5f4708015fe2a66a9d8ab60ae355b73d086b7957
Justin Lin <justinwlin@gmail.com>
2023-12-21 00:11:09 -0800

    Adding method to pass in audio_url

--- handler.py
+++ handler.py
@@ -5,6 +5,7 @@ import whisperx
 import gc 
 import base64
 import tempfile
+import requests
 
 # CHECK THE ENV VARIABLES FOR DEVICE AND COMPUTE TYPE
 device = os.environ.get('DEVICE', 'cuda') # cpu if on Mac
@@ -27,17 +28,37 @@ def base64_to_tempfile(base64_data):
 
     return temp_file.name
 
+def download_file(url):
+    """
+    Download a file from a URL to a temporary file and return its path.
+    """
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception("Failed to download file from URL")
+
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.mp3')
+    temp_file.write(response.content)
+    temp_file.close()
+    return temp_file.name
+
 def handler(event):
     '''
     Run inference on the model.
     Returns output path, width the seed used to generate the image.
     '''
     job_input = event['input']
-    job_input_audio_base_64 = job_input['audio_base_64']
-    if(job_input_audio_base_64 is None):
-        raise Exception("audio_base_64 is required")
-    audio_input = base64_to_tempfile(job_input_audio_base_64)
+    job_input_audio_base_64 = job_input.get('audio_base_64')
+    job_input_audio_url = job_input.get('audio_url')
 
+    if job_input_audio_base_64:
+        # If there is base64 data
+        audio_input = base64_to_tempfile(job_input_audio_base_64)
+    elif job_input_audio_url and job_input_audio_url.startswith('http'):
+        # If there is an URL
+        audio_input = download_file(job_input_audio_url)
+    else:
+        raise Exception("No audio input provided")
+    
     # 1. Transcribe with original whisper (batched)
     model = whisperx.load_model("small", device, compute_type=compute_type, language="en")
 
@@ -50,8 +71,6 @@ def handler(event):
     model_a, metadata = whisperx.load_align_model(language_code=language_code, device=device)
     result = whisperx.align(result["segments"], model_a, metadata, audio, device)
     print(result["segments"]) # after alignment
-
-
     return result
 
 runpod.serverless.start({
